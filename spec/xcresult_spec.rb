6cfbc9d96b3a992b987494dafe65b964c3080227
Aleksander Lorenc <aleksander.lorenc@lifeworks.com>
2024-08-21 12:19:04 +0200

    Removes real command execution.

--- spec/xcresult_spec.rb
+++ spec/xcresult_spec.rb
@@ -32,24 +32,24 @@ end
 RSpec.describe "XCResult Test Summaries" do
   let(:path) { File.absolute_path("./spec/fixtures/Test.xcresult") }
   let(:summary_id) { "0~2LVFAe2LWJ7FCnOsKan5UgGk7WChVJYuZlxPILKyxdpmfjsMrwjBJ2wUhaJQJ36Per_GRUfTI1cKeO2QiGvB8Q==" }
-  let!(:subject) { XCResult::Parser.new(path: path) }
 
   it 'has a version number' do
     expect(XCResult::VERSION).not_to be nil
   end
-  
+
   it 'parse xccov report paths' do
     require 'tmpdir'
     destination = Dir.mktmpdir
-  
+
     parser = XCResult::Parser.new(path: path)
     export_xccovreport_paths = parser.export_xccovreports(destination: destination)
-  
+
     expect(export_xccovreport_paths.count).to eq(1)  
   end
 
   it 'parse test plan summaries' do
-    summaries = subject.action_test_plan_summaries
+    parser = XCResult::Parser.new(path: path)
+    summaries = parser.action_test_plan_summaries
 
     testable_summary = summaries[0].summaries[0].testable_summaries[0]
 
