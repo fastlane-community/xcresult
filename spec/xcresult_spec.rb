aec49cb0fc6f4dd793595c70707b5d0fdc4bd1d1
Stephen Gurnett <stephen.gurnett@gmail.com>
2025-08-26 15:06:34 +0200

    Update unit test for legacy detection
    Update unit tests to confirm that the version is being correctly matched for the newer style string.
    
    .Include a test for the case where no version information can be decoded (assume new).

--- spec/xcresult_spec.rb
+++ spec/xcresult_spec.rb
@@ -35,7 +35,25 @@ RSpec.describe "XCResult Version" do
     it 'should have --legacy' do
       expect(command).to eq(expected)
     end
-  end  
+  end
+
+  context 'with Xcode 26 command line tools version style - non-legacy version' do
+    let(:xcresulttool_version) { 'xcresulttool version 23020.0, schema version: 0.1.0 (legacy commands format version: 3.53)' }
+    let(:expected) { "xcrun xcresulttool get --format json --path #{path} --id #{summary_id}" }
+
+    it 'should not have --legacy' do
+      expect(command).to eq(expected)
+    end
+  end
+
+  context 'with unsupported command line tools version' do
+    let(:xcresulttool_version) { 'no version here' }
+    let(:expected) { "xcrun xcresulttool get --legacy --format json --path #{path} --id #{summary_id}" }
+
+    it 'should have --legacy' do
+      expect(command).to eq(expected)
+    end
+  end
 end
 
 RSpec.describe "XCResult Test Summaries" do
